262d5abf0636c8201da16f2e2658358bd957277a
DaianSSFeliciano <Daiane.Feliciano@globalweb.com.br>
2021-10-27 14:29:43 -0300

    adc modulo

--- Funcao/Funcoes/funcoes-js/main5.js
+++ Funcao/Funcoes/funcoes-js/main5.js
@@ -1,3 +1,44 @@
-// tipo Number
-
+/*// tipo Number
+/*
 const meuNumero = 3;
+ const stringName = 'Alura';
+ const stringName3 = 'Alura';
+ console.log(stringName == stringName3);
+/*
+ let usuarioConectado= false;
+ console.log (String( usuarioConectado ) ); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+ usuarioConectado= true;
+ console.log (String( usuarioConectado ) ); // agora teremos uma string “true”.*/
+
+ /*
+ let usuarioConectado= false;
+ console.log (usuarioConectado.toString()); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+ usuarioConectado= true;
+ console.log (usuarioConectado.toString()); // agora teremos uma string “true”.
+
+let largura = '10';
+let altura = '5';
+console.log(+ largura * + altura); // teremos a conversão de String para números realizado usando o + antes das variáveis
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)*/
+
+var numero = null;
+
+console.log(numero)
+
+// tratamento de erro
+console.error('deu erro')
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
